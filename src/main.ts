58030ebc93b1a4c67a4737068e6ef811f690e16b
Stickman008 <arm.supakorn@gmail.com>
2023-09-16 09:42:47 +0700

    Update minor

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
+import { config } from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+config();
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
@@ -10,6 +13,7 @@ async function bootstrap() {
       options: {
         package: 'user',
         protoPath: 'humf-proto/user.proto',
+        url: process.env.GRPC_SERVER_URL,
       },
     }
   );
