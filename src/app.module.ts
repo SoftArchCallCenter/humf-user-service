6eae0034224cf77c498214e5d395b9182e668216
Stickman008 <arm.supakorn@gmail.com>
2023-09-20 14:43:57 +0700

    Update docker and clean code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
@@ -13,7 +11,7 @@ import { ConfigModule } from '@nestjs/config';
     AuthModule, 
     DatabaseModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
+  const port = process.env.USER_SERVICE_PORT || 5000;
+
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
