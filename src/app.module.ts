8c3700b06d2d1121246412173867785b59bcd892
PacharaWin <pacharawinboon@gmail.com>
2023-09-20 22:12:02 +0700

    fix module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,15 @@
-require("dotenv").config();
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
-import { User } from './users/entities/user.entity'
-import { TypeOrmModule } from '@nestjs/typeorm';
-
 
 @Module({
-  imports: [UsersModule, TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: process.env.MYSQL_HOST,
-    port: parseInt(process.env.MYSQL_PORT),
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    entities: [User],
-    synchronize: true,
-  })
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}), 
+    UsersModule, 
+    AuthModule, 
+    DatabaseModule
   ],
   controllers: [],
   providers: [],
