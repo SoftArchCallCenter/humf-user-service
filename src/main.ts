da24c9c0792b7434232715cf843677fffb6d443c
Stickman008 <69195590+Stickman008@users.noreply.github.com>
2023-11-09 21:52:59 +0700

    Fix docker bug

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { AtGuard } from './common/gaurds';
 
 async function bootstrap() {
   const port = process.env.USER_SERVICE_PORT || 5000;
-  const host = process.env.USER_SERVICE_host || "127.0.0.1";
+  const host = process.env.USER_SERVICE_HOST || "127.0.0.1";
 
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
