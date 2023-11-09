ef20ffe3361ef5dfd8d35d929f0f4b71c417caca
Stickman008 <arm.supakorn@gmail.com>
2023-11-09 21:12:39 +0700

    Update docker host

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ import { AtGuard } from './common/gaurds';
 
 async function bootstrap() {
   const port = process.env.USER_SERVICE_PORT || 5000;
+  const host = process.env.USER_SERVICE_host || "127.0.0.1";
 
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(port);
+  await app.listen(port, host);
 }
 bootstrap();
